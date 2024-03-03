29d0c1ccdaaa6a4b864391ff77525263415abf6d
IDB LAB-1 PC -6 <IDB LAB-1 PC -6@DESKTOP-TQDGPQA>
2024-03-03 18:40:53 +0600

    Add project files.

---
+++ ChatAppSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppSignalR.Hubs
+{
+	public class ChatHub : Hub
+	{
+		public async Task ShareText(string user, string message)
+		{
+			await Clients.All.SendAsync("msgRcv", user, message);
+
+		}
+
+		public async Task ShareImage(string user, string imageData)
+		{
+			await Clients.All.SendAsync("imgRcv", user, imageData);
+
+		}
+
+		public override Task OnConnectedAsync()
+		{
+			return base.OnConnectedAsync();
+		}
+
+		public override Task OnDisconnectedAsync(Exception? exception)
+		{
+			return base.OnDisconnectedAsync(exception);
+		}
+	}
+}
---
+++ ChatAppSignalR/wwwroot/js/chat.js
@@ -0,0 +1,67 @@
+﻿/// <reference path="../lib/microsoft/signalr/dist/browser/signalr.js" />
+
+'use strict';
+
+
+var username = prompt("enter your name");
+
+var hubBuilder = new signalR.HubConnectionBuilder();
+var connection = hubBuilder.withUrl("/chathub").withAutomaticReconnect().build();
+
+
+
+
+connection.on("msgRcv", function (user, message) {
+    var li = document.createElement("li");
+
+
+    li.textContent = `${user} : ${message}`;
+
+    $('#chatLog').append(li);
+
+
+});
+
+connection.on("imgRcv", function (user, imgData) {
+    
+    var img = document.createElement('img');
+
+    img.src = imgData;
+    var li = document.createElement('li');
+    var br = document.createElement('br');
+    li.append(user);
+    li.appendChild(br);
+    li.appendChild(img);
+    $('#chatLog').append(li);
+    
+});
+
+
+
+
+connection.start();
+
+
+$('#btnSend').on('click', function () {
+
+    var message = $('#txtInput').val();
+
+    connection.invoke("ShareText", username, message);
+    $('#txtInput').val(null);
+})
+
+
+
+$('#userFile').on('change', async (e) => {
+    // Get a reference to the file
+    const file = e.target.files[0];
+    
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+        this.connection.invoke("ShareImage", username, reader.result);
+    };
+    
+    $('#userFile').val('');
+});
+
