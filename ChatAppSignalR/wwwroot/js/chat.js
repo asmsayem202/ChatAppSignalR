1d428d35c43480f89973c53a30e887158d645e55
A. S. M. SAYEM <156167189+asmsayem202@users.noreply.github.com>
2024-03-04 11:54:03 +0600

    Update chat.js

--- ChatAppSignalR/wwwroot/js/chat.js
+++ ChatAppSignalR/wwwroot/js/chat.js
@@ -14,8 +14,8 @@ var connection = hubBuilder.withUrl("/chathub").withAutomaticReconnect().build()
 connection.on("msgRcv", function (user, message) {
     var li = document.createElement("li");
 
-
-    li.textContent = `${user} : ${message}`;
+    li.innerHTML = `<b>${user}</b> : <i>${message}</i>`;
+    /*li.textContent = `${user} : ${message}`;*/
 
     $('#chatLog').append(li);
 
@@ -23,17 +23,18 @@ connection.on("msgRcv", function (user, message) {
 });
 
 connection.on("imgRcv", function (user, imgData) {
-    
+
     var img = document.createElement('img');
 
     img.src = imgData;
     var li = document.createElement('li');
     var br = document.createElement('br');
-    li.append(user);
+    li.innerHTML = `<b>${user}</b>`;
+    /* li.append(user);*/
     li.appendChild(br);
     li.appendChild(img);
     $('#chatLog').append(li);
-    
+
 });
 
 
@@ -55,13 +56,13 @@ $('#btnSend').on('click', function () {
 $('#userFile').on('change', async (e) => {
     // Get a reference to the file
     const file = e.target.files[0];
-    
+
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => {
         this.connection.invoke("ShareImage", username, reader.result);
     };
-    
+
     $('#userFile').val('');
 });
 
